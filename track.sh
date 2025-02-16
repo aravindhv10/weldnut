52c7f96cce44681c2480a68a17a7e8089b41eafd
Your Name <you@example.com>
2025-02-16 12:05:25 +0530

    More samurai code

--- track.sh
+++ track.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 . "${HOME}/BEN2/bin/activate"
-python3 "${HOME}/GITHUB/yangchris11/samurai/scripts/demo.py"
---video_path <your_video.mp4> --txt_path <path_to_first_frame_bbox.txt>
+VIDEO_FILE="$(realpath "${1}")"
+TEXT_BBOX_FILE="$(realpath "${2}")"
+cd "${HOME}/GITHUB/yangchris11/samurai"
+python3 "${HOME}/GITHUB/yangchris11/samurai/scripts/demo.py" \
+    --video_path "${VIDEO_FILE}" \
+    --txt_path "${TEXT_BBOX_FILE}" \
+;
